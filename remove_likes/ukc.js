7d94535dabadfd499faf037f1cea60743ac0de39
Alan Saul <alan.daniel.saul@gmail.com>
2016-01-18 23:17:43 +0000

    Tidy up

--- remove_likes/ukc.js
+++ remove_likes/ukc.js
@@ -1,11 +1,4 @@
 function remove_likes(){
-    console.log("Removing likes");
     $('#threadText .btn-group .btn-sm').hide()
-    //$('#threadText .btn-group .btn-sm').
-    //// #threadText .btn-group .btn-sm
-    //'*[(@id = "threadText")]//*[contains(concat( " ", @class, " " ), concat( " ", "btn-group", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "btn-sm", " " ))]'
-    //console.log("All likes are being removed");
-    //setTimeout(function(){remove_likes();}, 2000);  // Run every two seconds
 }
-console.log("loaded");
 remove_likes()
